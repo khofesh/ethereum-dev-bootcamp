f405416d8d592410810dafafac1e98bee1d2a9fb
fahmi ahmad <fahmiahmad@tutanota.com>
2021-10-10 20:14:51 +0700

    fix errors

--- section-6/index.js
+++ section-6/index.js
@@ -1,9 +1,9 @@
 import Web3, { providers } from "web3";
 
 // web3 instance
-const web3 = new Web3(providers.HttpProvider("http://127.0.0.1:7545"));
+const web3 = new Web3(new providers.HttpProvider("http://127.0.0.1:7545"));
 
 web3.eth
   .getBalance("0x7D481adF27E28538b3EE123ab7C990CFa62a6629")
-  .then((val) => console.log)
+  .then((val) => console.log(val))
   .catch((err) => console.log(err));
