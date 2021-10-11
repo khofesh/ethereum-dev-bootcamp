266b14ed9a645ad0cd835d60eb9fd43ee2c032c3
fahmi ahmad <fahmiahmad@tutanota.com>
2021-10-11 11:14:14 +0700

    interact with remix

--- section-6/index.js
+++ section-6/index.js
@@ -3,8 +3,51 @@ import Web3, { providers } from "web3";
 // web3 instance
 const web3 = new Web3(new providers.HttpProvider("http://127.0.0.1:7545"));
 
-const source = "0x7D481adF27E28538b3EE123ab7C990CFa62a6629";
-const destination = "0xCbc72A600686a98caF345D349bd7944137e6B751";
+const source = "0xbC9c84097A7D0366509eE00a67236fC32E14bDCB";
+const destination = "0xf8e81D47203A594245E36C48e151709F0C19fBe8";
+const contractAddress = "0xf8e81D47203A594245E36C48e151709F0C19fBe8";
+
+const remixABI = [
+  {
+    inputs: [],
+    name: "getMyUint",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "number",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "myUint",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_myUint",
+        type: "uint256",
+      },
+    ],
+    name: "setUint",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
 
 async function getBalance(address) {
   try {
@@ -44,7 +87,52 @@ async function sendTransaction(from, to, amount) {
   }
 }
 
+async function ethCall(from, to, data) {
+  try {
+    const result = await web3.eth.call({ from, to, data });
+
+    return {
+      status: true,
+      result,
+    };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      status: false,
+    };
+  }
+}
+
+async function ethCall2() {
+  try {
+    const myContract = new web3.eth.Contract(remixABI, contractAddress, {
+      from: source,
+    });
+    const sendResult = await myContract.methods.setUint(50).send();
+    const result = await myContract.methods.window.getMyUint().call();
+
+    return {
+      status: true,
+      sendResult,
+      result,
+    };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      status: false,
+    };
+  }
+}
+
 (async () => {
-  const result = await sendTransaction(source, destination, "1");
+  // const result = await sendTransaction(source, destination, "1");
+
+  // const hexCode = web3.utils.sha3("myUint()").substr(0, 10);
+  // const result = await ethCall(source, destination, hexCode);
+
+  const result = await ethCall2();
+
   console.log(result);
 })();
